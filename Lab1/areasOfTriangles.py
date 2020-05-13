b08c7a2561c0442b52a98a942182b25939ba5f0b
jerschne <jerschne@iu.edu>
2020-05-13 12:58:47 -0400

    working on a areasOfTriangle towards the end of lab

---
+++ Lab1/areasOfTriangles.py
@@ -0,0 +1,4 @@
+#returns a value which is a string
+#use this later for Assignment1 to solve the triangle problem-set
+a = input("please enter the length of the first side: ")
+print("you have entered: " + a) 
