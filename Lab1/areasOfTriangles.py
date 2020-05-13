5ce3756926f0c9570e6ac2893cd39590497517bd
jerschne <jerschne@iu.edu>
2020-05-13 13:05:15 -0400

    the end of Lab1

--- Lab1/areasOfTriangles.py
+++ Lab1/areasOfTriangles.py
@@ -2,3 +2,9 @@
 #use this later for Assignment1 to solve the triangle problem-set
 a = input("please enter the length of the first side: ")
 print("you have entered: " + a) 
+#this raises a by the square root
+#the issue with this is whatever number I enter becomes entered as a string rather than a integerto solve this: apply float to a
+b = float(a) ** 0.5
+print("the square root of "+a+" is "+str(b)+"")
+
+
