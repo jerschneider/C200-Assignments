ee5445abd78caf72b69c7c3c43abc3e6d0e08305
jerschne <jerschne@iu.edu>
2020-05-14 12:47:35 -0400

    saving progress as lab it about to end

---
+++ Lab2/areasOfTriangle_v2.py
@@ -0,0 +1,18 @@
+#I entered these lines from Mr. German's lab page with my own edits/comments
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+a = input("Please enter the length of the first side of the triangle >>> ")
+#reminder float makes (a) a integer that's usebale through float, while string changes the integer to a string type
+a = float(a) 
+b = float(input("... now the second >>> ")) # in one step (b is never a string) this is a shortned version of (a)
+c = float(input("... and the third >>> ")) # careful with the parens(?)
+print("You have entered: " + str(a) + ", " +str(b) + ", and " + str(c))
+#we need to keep the user (ourselves, at this point) in the loop
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Heron's Forumula (imported from Mr. German's notes)
+#\/ below you must import the math module to use the square root command
+import math
+s = (a + b + c) / 2
+area = ( s * (s - a) * (s - b) * (s - c) ) ** 0.5
+#area = math.sqrt( s * (s - a) * (s - b) * (s - c))
+#^ the math command----> math.whatevercommand --->this command specifically calls the math module
+print("The area for the triangle with those sides is: " + str(area))
