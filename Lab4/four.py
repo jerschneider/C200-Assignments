b12446cf863a2fa4eda20657689b4837853c32f3
jeremy schneider <jeremy@tpx>
2020-05-18 13:00:25 -0400

    a scalable (N) made from '*'

--- Lab4/four.py
+++ Lab4/four.py
@@ -1,8 +1,12 @@
 size = input("What's the size: ")
 for i in range(int(size)): #i counts the lines (horizontal rows)
-    row =""
+    row = ""
     for j in range(int(size)): # j counts the column (in a row)
-        row = "*" + row
+        if j == 0 or i == j or j == int(size)-1: 
+            row = row + "* "
+        else:
+            row = row + "  "
+
     print(row)
     #print(str(i) + ": " + row)
 # this program produces a string of stars in a loop
