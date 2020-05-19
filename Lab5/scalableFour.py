f9f597974a6f9c9ba50d69080e4a3a537f8373af
jeremy schneider <jeremy@tpx>
2020-05-19 12:46:44 -0400

    scaleable X is finished

--- Lab5/scalableFour.py
+++ Lab5/scalableFour.py
@@ -1,1 +1,11 @@
-print("Howdy")
+#this is a fairly clunky ---> 15size makes it a full X
+size = int(input("What size: "))
+for line in range(size):
+    row = ""
+    for column in range(size): #print "****"
+        if line == column or line+column == size-1: #line == 0 or column == size-1 or line == size-1 or column == 0:
+            row = row + "* " 
+        else: 
+            row = row + " " # row = row + "(" + str(line) + ", " + str(column) + ")"
+    print(row)
+
