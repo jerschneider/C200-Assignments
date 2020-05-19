e8338191b98084f76739616b451ac6476219291d
jeremy schneider <jeremy@tpx>
2020-05-18 20:32:37 -0400

    problem 1 of Assignment2 is alsmost done and i just started on the second problem

---
+++ Assignment2/old.py
@@ -0,0 +1,22 @@
+import math
+
+#Problem 1
+#tomato will be passed through this function
+def themath(tomato):
+    Themath = (tomato *150)-15
+    return Themath
+
+print("Assignment 1 Problem 2: " + str(themath(10)))
+
+#tomato = 10
+#potato = (tomato * 150)-15
+#print("potato is:")
+#print(potato)
+
+#theLast = math.sqrt(potato)
+#print("theLast is:")
+#print(theLast)
+
+#Problem 2
+#Problem 3
+#Problem 4
