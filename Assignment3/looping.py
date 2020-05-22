4c08e8f6cb113cdef550d9e74d2641164a1475ee
jerschne <jerschne@iu.edu>
2020-05-21 22:57:51 -0400

    finished problem 1 of Assignment3

--- Assignment3/looping.py
+++ Assignment3/looping.py
@@ -1,19 +1,12 @@
 def minVal(theLst):
-    MinVal = theLst[0] #this assigns the minium value to be 0
+    MinVal = theLst[0] #this references the (0) position of a container and then goes forward from there
     for var in theLst:
-        if var < MinVal: #this states that if a variable is less than the MinVal then the MinVal is var
-            MinVal = var
+        if var < MinVal: 
+            MinVal = var #I redefine the variable(MinVal, so its now the least value in terms of a integer 
     return MinVal
-    #"""
-    #Given a list, determine the minimum value in the list
-    #
-    #Input: A list numbers of any length (at least 1 item in the list)
-    #
-    #Return: Value of minimum value
-    #
-    #Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-    #"""
-    #pass
+#a list is a series of integers, and are ranked by their position
+ #["red", "red", "blue", "x", 2, "boop"]
+  #  0       1       2   3   4       5
 
 
 def minValIndex(theLst):
@@ -30,67 +23,66 @@ def minValTuple(theLst):
     for var in theLst:
         if var < MinVal: 
             MinVal = var
-    MinValIndex = theLst.index(MinVal)
+    MinValIndex = theLst.index(MinVal) 
     tup1 = (MinVal, MinValIndex)
     MinValTuple = tup1
     return MinValTuple
    
-   
-   
-   
-   
-    
 
 def maxVal(theLst):
-    """
-    Given a list, determine the maximum value in the list
-
-    Input: A list numbers of any length (at least 1 item in the list)
-
-    Return: Value of maximum value
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-    """
-    pass
+    MaxVal = theLst[0]
+    for var in theLst:
+        if var > MaxVal: #this states that if a variable is less than the MinVal then the MinVal is var
+            MaxVal = var
+    return MaxVal
 
 def maxValIndex(theLst):
-    """
-    Given a list, determine the index of the maximum value (the last occurence)
-
-    Input: A list numbers of any length (at least 1 item in the list)
-
-    Return: Index of the largest value (the last occurence)
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-    """
-    pass
+    MaxVal = theLst[0]
+    for var in theLst:
+        if var > MaxVal: 
+            MaxVal = var
+    MaxValIndex = theLst.index(MaxVal)
+    return MaxValIndex
+    
 
 def maxValTuple(theLst):
-    """
-    Given a list, determine the maximum value and index in the list (the last occurence)
-
-    Input: A list numbers of any length (at least 1 item in the list)
-
-    Return: Tuple, first item is the largest value, second item is the index of the value (the last occurence)
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-    """
-    pass
-
+    MaxVal = theLst[0] 
+    for var in theLst:
+        if var > MaxVal: 
+            MaxVal = var
+    MaxValIndex = theLst.index(MaxVal) 
+    tup1 = (MaxVal, MaxValIndex)
+    MaxValTuple = tup1
+    return MaxValTuple
+   
 def tripTotal(logs):
-    """
-    Given a list containing sublists of size 2, each sublist 
-    [number of miles traveled, speed traveled at that distance]. 
-    You will find the total time of the trip
+    time = 0
+    for var in logs:
+        time = time + (var[0] / var[1]) # i use time a second 'time' here, instead of totalTime because 'time = ' is needed because of how the program counts as it goes through the list
+        totalTime = time #infact this line is completey uneeded (do not do this)
+    return totalTime
+#This probelm was a learning experience for me and I truly learned how for loops work
+# In fact below is what I tried to do at first (and a few other failed attempts)
+# THE BIGGEST TAKEAWAY FROM THIS PROBLEM IS THAT VAR IS EACH INDIVIDUAL ITEM IN A List given to a 'FOR LOOP' (but it could also be anything)
+# 
+# 
+#  if var0 = logs.index[0]:
+#            list0 = var0
+#            list0Time = (var0.index(0) / var0.index(1))
+#       if var1 = logs.index[1]:
+#            list1 = var1
+#            list1Time = (var1.index(0) / var1.index(1))
+#        if var2 = logs.index[2]:
+#            list2 = var2
+#            list2Time = (var2.index(0) / var2.index(1))
+#        trip = list0Time + list1Time + list2Time
+#        TripTotal = trip
+#        return TripTotal
 
-    Input: List of lists, containing at least 1 item.
-    [[m1, s1], [m2, s2], [m3, s3], ...]
 
-    Return: The total time the trip took.
 
-    Required to use a single for loop
-    """
-    pass
+
+
 
 
 if __name__ == "__main__":
