94f9df51e1c378b80ea9e5d88c82e9f114616bd5
jerschne <jerschne@iu.edu>
2020-05-23 00:18:39 -0400

    FINISHED! :)

--- Assignment3/whileString.py
+++ Assignment3/whileString.py
@@ -1,40 +1,58 @@
 def palindrome(x):
+    i = 0
     string = x 
     length = len(string)
-    for var in range(length):
-        if (string[var] != string[length - var -1]): #!= (does not equal) checks to see
+    while i < length:
+        if (string[i] != string[length - i -1]):
+            string = i
             return False
+        i = i + 1 
     return True
+    
+    
+    
+    """
+    For a given string, determine if the string is a palindrome. (The same forward and back)
 
-#    if(x==x[::-1]):
-#        x = True
-#    else:
-#        x = False
-#   return x
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    for c in x:
-#        print(c)
-#    return True
-   #answer = True
-   #for i in range(len(x)):
-       #print("-->", x[i])
-       #if not (x[i] == x[(len(x)-1)-i]):
-           #check character at i against the chracter i positions to the left of the last character
-       #    premature optimization is the root of all evil
-       #    what about negative indexing? -->says Mr. German
-       # madam
-       # 01234 4 == len("madam") - 1
-       #         return False
-    #return True
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Input: String of any size (including empty)
+
+    Return: Boolean (True if palindrome, False otherwise)
+
+    Limitation: You must use a loop. Only functions allowed to be used is len or range (if needed). Cannot use "in" besides "for var in container"
+    """
+    #pass
+
+
 def getCount(char, strng):
-    count = 0 #set up a counter which I used in looping
-    for i in range(len(strng)): #but combined the previous functions setup to solve this. 
-        if(strng[i] == char):
+    i = 0
+    count = 0
+    while i < len(strng): # deleted the range which I had with the forloop variant
+        if (strng[i] == char):
             count = count + 1
+        i = i + 1
     return count
+    
+    """
+    Get the number of times a character appears in a string.
+
+    Input: A character (length 1) and a string of any length
+
+    Return: Number of times a character appears
+
+    Limitation: Only functions we have been taught in class. Must use a loop. Cannot use "in" besides "for var in container"
+    """
+   # pass
 
 def getIndex(char, strng):
+    i = 0
+    index = 0
+    while i < len(strng):
+        if char == strng[i]:
+            index = i
+            return index
+        i = i + 1
+    return len(strng)
+    
     """
     Gets the index of the character in the string
 
@@ -45,14 +63,23 @@ def getIndex(char, strng):
 
     Limitation: Only functions we have been taught in class. Must use a loop. Cannot use "in" besides "for var in container"
     """
-    #pass 
-
-    for var in range(len(strng)):
-        if char == strng[var]:
-            return var
-    return len(strng)
+   # pass 
 
 def areEqual(str1, str2):
+    i = 0
+    count = 0
+    if len(str1) != len(str2):
+        return False
+    while i < len(str1):
+        if str1[i] == str2[i]:
+            count = i  
+            pass
+        else:
+            return False
+        i = i + 1 #had to move this after the else statement
+    return True
+
+   
     """
     Determines if 2 strings are equal
 
@@ -62,22 +89,7 @@ def areEqual(str1, str2):
 
     Limitation:  Must use a single loop, and only 1 return statement in the code. Cannot use "in" besides "for var in container"
     """
-   #This is leo's code from lab which was incorrect because it didnt loop 
-   # pass
-   # if str1 == str2
-    #    return True
-    #else:
-     #   return False
-    #
-    #This is the code from lab which Mr. German suggested as an alternative to Leo's approach
-    if len(str1) != len(str2):
-        return False
-    for i in range(len(str1)):
-        if str1[i] == str2[i]:
-            pass #this stops the loop
-        else:
-            return False
-    return True
+    #pass
 
 
 if __name__ == "__main__":
@@ -114,5 +126,3 @@ if __name__ == "__main__":
     print("Actual: ", areEqual(s1, s2), "Expected: ", False)
     print("Actual: ", areEqual(s3, s2), "Expected: ", False)
     print("Actual: ", areEqual(s1, s1), "Expected: ", True)
-    #test code from lab
-    print(" Tom Tomato Cruise >>> Actual: ", areEqual("Tom", "Tomato"), " Expected: ", False)
