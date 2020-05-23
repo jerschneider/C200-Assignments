ac4f565bd97f207083fdd891904447be65eb58e4
jerschne <jerschne@iu.edu>
2020-05-22 22:10:30 -0400

    beep boop

--- Assignment3/whiling.py
+++ Assignment3/whiling.py
@@ -8,76 +8,18 @@ def minVal(theLst):
        i = i + 1 # this is the same as >>>> i += 1 
    return MinVal
 
-#def minVal(theLst):
-#    MinVal = theLst[0] #this references the (0) position of a container and then goes forward from there
-#    for var in theLst:
-#        if var < MinVal: 
-#            MinVal = var #I redefine the variable(MinVal, so its now the least value in terms of a integer 
-#    return MinVal
-
-
-#    for var in theLst:
-#        if var < MinVal: 
-#            MinVal = var
-#    return MinVal
-"""
-    Given a list, determine the minimum value in the list
-
-    Input: A list numbers of any length (at least 1 item in the list)
-
-    Return: Value of minimum value
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-"""
-   # pass
 
 def minValIndex(theLst):
    i = 0 
    MinVal = theLst[0]
    while i < len(theLst):
-       if theLst[i] < MinVal:
+       if theLst[i] <= MinVal:
            MinVal = theLst[i]
            minValIndex = i
        i = i + 1  
    return minValIndex
 
 
-"""
-def minValIndex(theLst):
-    Index = 0
-    MinVal = theLst[0] #starting point is at position 0 for the forloop
-    for var in theLst:
-        if var <= MinVal: # <= this is used to help with my counting of the indexes
-            MinVal = var
-            MinValIndex = Index #this records the current index
-        Index = Index + 1 #as the forloop loops then this will go up (or counts)
-    return MinValIndex
-
-
-def minVal(theLst):
-   #i used the count method here to port over my for loop code
-   i = 0 
-   MinVal = theLst[0]
-   while i < len(theLst):
-       if theLst[i] < MinVal:
-           MinVal = theLst[i]
-       i = i + 1 # this is the same as >>>> i += 1 
-   return MinVal
-"""
-
-
-
-
-"""
-    Given a list, determine the index of the minimum value (the last occurence)
-
-    Input: A list numbers of any length (at least 1 item in the list)
-
-    Return: Index of the smallest value (the last occurence)
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-"""
-    #pass
 
 def minValTuple(theLst):
     i = 0 
@@ -91,18 +33,6 @@ def minValTuple(theLst):
         MinValTuple = tup1 
     return MinValTuple
 
-    
-    
-    """
-    Given a list, determine the minimum value and index in the list
-
-    Input: A list numbers of any length (at least 1 item in the list)
-
-    Return: Tuple, first item is the smallest value, second item is the index of the value (the last occurence)
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-    """
-   # pass
 
 def maxVal(theLst):
     i = 0 
@@ -113,69 +43,42 @@ def maxVal(theLst):
         i = i + 1  
     return MaxVal
     
-    
-    """
-    Given a list, determine the maximum value in the list
-
-    Input: A list numbers of any length (at least 1 item in the list)
-
-    Return: Value of maximum value
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-    """
-   # pass
 
 def maxValIndex(theLst):
+   maxValIndex = 0 #  I had to add this to get this code to work (even though the setup worked for minValIndex). BECAUSE It doesnt set the index if the first item is the maxium
+   i = 0 
+   MaxVal = theLst[0]
+   while i < len(theLst):
+       if theLst[i] >= MaxVal:
+           MaxVal = theLst[i]
+           maxValIndex = i 
+       i = i + 1
+   return maxValIndex
+    
+    
+
+def maxValTuple(theLst):
     i = 0 
     MaxVal = theLst[0]
     while i < len(theLst):
-        if theLst[i] > MaxVal:
+        if theLst[i] >= MaxVal:
             MaxVal = theLst[i]
             maxValIndex = i
         i = i + 1
-    return maxValIndex
-    
-    
-    
-    
-    
-"""
-    Given a list, determine the index of the maximum value (the last occurence)
+        tup2 = (MaxVal, maxValIndex)
+        MaxValTuple = tup2 
+    return MaxValTuple
 
-    Input: A list numbers of any length (at least 1 item in the list)
 
-    Return: Index of the largest value (the last occurence)
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-"""
-   # pass
-
-def maxValTuple(theLst):
-    """
-    Given a list, determine the maximum value and index in the list (the last occurence)
-
-    Input: A list numbers of any length (at least 1 item in the list)
-
-    Return: Tuple, first item is the largest value, second item is the index of the value (the last occurence)
-
-    Limitation: The only function you can use (if you want to) is `len()` and `range()`. Cannot use "in" besides "for var in container"
-    """
-    pass
 
 def tripTotal(logs):
-    """
-    Given a list containing sublists of size 2, each sublist 
-    [number of miles traveled, speed traveled at that distance]. 
-    You will find the total time of the trip
-
-    Input: List of lists, containing at least 1 item.
-    [[m1, s1], [m2, s2], [m3, s3], ...]
-
-    Return: The total time the trip took.
-
-    Required to use a single for loop
-    """
-    pass
+   i = 0
+   time = 0
+   while i < len(logs):
+       var = logs[i] # for var in logs: >>>> in the for loop var is automatically var = log[i]
+       time = time + (var[0] / var[1])
+   i = i + 1 
+   return time
 
 
 if __name__ == "__main__":
