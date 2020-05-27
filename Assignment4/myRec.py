e09192ec39f5c0cc43640922baf52cbd708daa5b
jerschne <jerschne@iu.edu>
2020-05-27 00:22:47 -0400

    super close to being super done  : ^ )

--- Assignment4/myRec.py
+++ Assignment4/myRec.py
@@ -8,22 +8,6 @@ def function1(n):
         n = n - 1
     return 0
 
-#TestCode
-print("")
-print("===function1===")
-print( function1 (11) )
-print( function1 (10) )
-print( function1 (9) )
-print( function1 (8) )
-print( function1 (7) )
-print( function1 (6) )
-print( function1 (5) )
-print( function1 (4) )
-print( function1 (3) )
-print( function1 (2) )
-print( function1 (1) )
-print("===END===")
-print ("")
 
 def closedFunct1(n):
     n = n
@@ -32,22 +16,6 @@ def closedFunct1(n):
         n = n - 1
     return 0
 
-print("")
-print("===closedFunct1===")
-print( closedFunct1 (11) )
-print( closedFunct1 (10) )
-print( closedFunct1 (9) )
-print( closedFunct1 (8) )
-print( closedFunct1 (7) )
-print( closedFunct1 (6) )
-print( closedFunct1 (5) )
-print( closedFunct1 (4) )
-print( closedFunct1 (3) )
-print( closedFunct1 (2) )
-print( closedFunct1 (1) )
-print("===END===")
-print ("")
-
 
 def funct2(n):
     n = n
@@ -55,22 +23,6 @@ def funct2(n):
         return ( (n - 1) + 0.02 * (n - 1))
         n = n - 1
 
-print("")
-print("===funct2===")
-print( funct2 (13) )
-print( funct2 (12) )
-print( funct2 (11) )
-print( funct2 (10))
-print( funct2 (9) )
-print( funct2 (8) )
-print( funct2 (7) )
-print( funct2 (6) )
-print( funct2 (5) )
-print( funct2 (4) )
-print( funct2 (3) )
-print( funct2 (2) )
-print("===END===")
-print("")
 
 def closedFunc2(n):
     
@@ -79,24 +31,6 @@ def closedFunc2(n):
         return (1.02)**n * 10000
         n = n - 1
     return 10000
-
-print("")
-print("===closedFunc2===")
-print( closedFunc2 (13) )
-print( closedFunc2 (12) )
-print( closedFunc2 (11) )
-print( closedFunc2 (10))
-print( closedFunc2 (9) )
-print( closedFunc2 (8) )
-print( closedFunc2 (7) )
-print( closedFunc2 (6) )
-print( closedFunc2 (5) )
-print( closedFunc2 (4) )
-print( closedFunc2 (3) )
-print( closedFunc2 (2) )
-print( closedFunc2 (0) )
-print("===END===")
-print("")
 
 
 def func3(n):
@@ -108,20 +42,7 @@ def func3(n):
             return (n - 1) + (n -2)
         n = n - 1
     return 2
-print("")
-print("===func3===")
-print( func3 ( 10 ) )
-print( func3 (9) )
-print( func3 (8) )
-print( func3 (7) )
-print( func3 (6) )
-print( func3 (5) )
-print( func3 (4) )
-print( func3 ( 3 ) )
-print( func3 ( 2 ) )
-print( func3 ( 1 ) )
-print("===END===")
-print("")
+
 
 def func4(n):
     if n == 1:
@@ -131,20 +52,6 @@ def func4(n):
         return 9 * (n - 1) + (10 ** (n - 1)) - (n - 1)
         n = n - 1
 
-print("")
-print("===func4===")
-print( func4 (10) )
-print( func4 (9) )
-print( func4 (8) )
-print( func4 (7) )
-print( func4 (6) )
-print( func4 (5) )
-print( func4 (4) )
-print( func4 ( 3 ) )
-print( func4 ( 2 ) )
-print( func4 ( 1 ) )
-print("===END===")
-print("")
 
 def func5(n):
     if n == 0:
@@ -154,19 +61,7 @@ def func5(n):
         return 3 * (n - 1) + 1
         n = n - 1
     return 3
-print("")
-print("===func5===")
-print( func5 (10) )
-print ( func5 (9))
-print ( func5 (8))
-print ( func5 (7))
-print ( func5 (6))
-print ( func5 (5))
-print ( func5 (4))
-print ( func5 (3))
-print ( func5 (2))
-print ( func5 (1))
-print ( func5 (0))
+
 
 def closedFunc5(n):
     if n == 0:
@@ -176,19 +71,7 @@ def closedFunc5(n):
         return ((3 ** (n+1)) -1) / 2
         n = n - 1
     return 2
-print("")
-print("===ClosedFunc5===")
-print( closedFunc5 (10) )
-print( closedFunc5 (9) )
-print( closedFunc5 (8) )
-print( closedFunc5 (7) )
-print( closedFunc5 (6) )
-print( closedFunc5 (5) )
-print( closedFunc5 (4) )
-print( closedFunc5 (3) )
-print( closedFunc5 (2) )
-print( closedFunc5 (1) )
-print( closedFunc5 (0) )
+
 
 
 
