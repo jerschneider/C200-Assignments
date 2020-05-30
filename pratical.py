24d234b4d6f869192c343ced69d672c5ebd5bae0
jerschne <jerschne@iu.edu>
2020-05-30 13:46:11 -0400

    bombed the practical

---
+++ pratical.py
@@ -0,0 +1,12 @@
+import randrange
+def dice(n):
+    histogram = {}
+     
+    for i in range(n):
+        # i need throw the dice
+
+        #i = i + 
+        #rint("i am throwin the dice", i )
+
+print(dice(1000))
+        
