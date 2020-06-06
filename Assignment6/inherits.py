876420c690b26d1c3c65cfafb51774e3f3acca45
jerschne <jerschne@iu.edu>
2020-06-06 00:30:37 -0400

    getting started

---
+++ Assignment6/inherits.py
@@ -0,0 +1,15 @@
+#design a course
+#create assignments
+    ##this is going to be a class
+    #maybe a superclass
+    #quizzes, exams, labs and homework, and attendance
+
+    ##things to consider for Assignments >>> the 
+#
+
+
+
+##superclass
+#superclass is course
+#subclass is assignemnts
+#dictionary of students
