91f60727c3893db0f90f438b3be1cde99751d536
jerschne <jerschne@iu.edu>
2020-06-06 00:37:43 -0400

    boop

--- Assignment6/inherits.py
+++ Assignment6/inherits.py
@@ -12,4 +12,13 @@
 ##superclass
 #superclass is course
 #subclass is assignemnts
+
 #dictionary of students
+
+class course(): 
+    def __init__(self): #lets begin with just self
+        students = {} #dictionary for student pool ## and their grades
+    def addStudent(self, name): # start every student off with a grade  of 0, we will be doing this in percents
+        students[name] = 0
+        
+
