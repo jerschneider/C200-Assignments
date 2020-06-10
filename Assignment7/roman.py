e41221cce96c8fdd6d9184ac8deb63f26bdef162
jerschne <jerschne@iu.edu>
2020-06-09 21:44:45 -0400

    started on inherits, but now tackling roman nums

---
+++ Assignment7/roman.py
@@ -0,0 +1,27 @@
+#create a function that takes a number and converts it to a roman numeral
+# hint: task is made easier if you use modulo and integer divsion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##TestCode
+#use :
+##__name__ == "__main__"
