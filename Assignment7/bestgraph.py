034e01b344c479d1485c152c4b831234ed9474c2
jerschne <jerschne@iu.edu>
2020-06-10 19:25:11 -0400

    testing stuff for graph

--- Assignment7/bestgraph.py
+++ Assignment7/bestgraph.py
@@ -9,14 +9,20 @@ class Graph:
     def add_edge(self, pair):
         start, end = pair # breaking apart a tuple of 2 items
         self.edges[start].append(end)
-    
+    def del_edge(self, pair):
+
     def children(self):
         return self.edges[node]
     
     def nodes(self):
         # We just happen to have an instance variable
         #   the same name as a function 
-        return str(self.nodes) 
-    
+        return str(self.nodes)
+    def add_node(self):
+
+    def del_node(self):
+
+    def adjMatrix():
+        #prdocues a list of lists that is an N x N matrix
     def __str__(self):
         return str(self.edges)
