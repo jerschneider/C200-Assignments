03c9a3910a74e3ce44a7568063ef2b45ff2b0750
jerschne <jerschne@iu.edu>
2020-06-10 20:49:26 -0400

    going through graph code and picking it apart

--- Assignment7/bestgraph.py
+++ Assignment7/bestgraph.py
@@ -1,25 +1,25 @@
 class Graph:
 
     def __init__(self, nodes):
-        self.nodes = nodes
-        self.edges = {}
-        for i in self.nodes:
-            self.edges[i] = []
+        self.nodes = nodes #this is the list
+        self.edges = {} #this is the dictionary for the lists of edges
+        for node in nodes:
+            self.edges[node] = [] #creates a list inside of the dictionary, a key where the value is an empty list
     
     def add_edge(self, pair):
         (start, end) = pair # breaking apart a tuple of 2 item
         if start not in self.nodes or end not in self.nodes:
-            return -1
-        if end not in self.edges[start]:
-            self.edges[start].append(end)
+            return -1 # a false
+        if end not in self.edges[start]: #edges is the dictionary for each path
+            self.edges[start].append(end) #adds a new or an additional end value to an existing key >>> start:[existingpaths]+newpath
             return 1
         else:
             return -1    
-    def del_edge(self, pair):
-        (one, two) = pair
-        if one in self.nodes and two in self.nodes:
+    
+    def del_edge(self, pair): #a method dedicated to just deleting
+        (one, two) = pair #maybe change this to (start, end) for consistency
+        if one in self.nodes and two in self.nodes and two in self.edges[one]:
             self.edges[one].remove(two)
-
         else:
             return -1
 
@@ -28,16 +28,17 @@ class Graph:
 
         #del self.edges[start].append(end)
 
-    def children(self):
-        return self.edges[node]
+    #def children(self):
+        #return self.edges[node]
     
-    def nodes(self):
+    #def nodes(self):
         # We just happen to have an instance variable
         #   the same name as a function 
-        return str(self.nodes)
+        #return str(self.nodes)
+    
     def add_node(self, node):
         if node in self.nodes:
-            return -1
+            return -1 #returns -1(false) if the node already exists
         else:
             self.nodes += [node]
             self.edges[node] = []
@@ -56,8 +57,23 @@ class Graph:
     
     def adjMatrix(self):
         #prdocues a list of lists that is an N x N matrix
+        #pass
+        result = []
+        for start in self.edges.keys():
+            row = []
+            for end in self.edges.keys():
+                if end in self.edges[start]:
+                    row += [1]
+                else:
+                    row += [0]
+            result.append(row)
+        return result
     
-    
-    #def __str__(self):
-        #return str(self.edges)
+    #Mr german had this included in his example and Iam not sure what it does
+    def __repr__(self):
+        return str(self.nodes) + " " + str(self.edges)
+
+
+    def __str__(self):
+        return str(self.edges)
         
