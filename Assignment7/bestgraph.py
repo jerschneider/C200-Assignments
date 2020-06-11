c8a028bfe0125eff6c2c24bf8c864d02bd29390a
jerschne <jerschne@iu.edu>
2020-06-10 20:03:01 -0400

    getting somewhere with graphs (thank you mr. german for the office hours video, it's been very helpful

--- Assignment7/bestgraph.py
+++ Assignment7/bestgraph.py
@@ -7,9 +7,26 @@ class Graph:
             self.edges[i] = []
     
     def add_edge(self, pair):
-        start, end = pair # breaking apart a tuple of 2 items
-        self.edges[start].append(end)
+        (start, end) = pair # breaking apart a tuple of 2 item
+        if start not in self.nodes or end not in self.nodes:
+            return -1
+        if end not in self.edges[start]:
+            self.edges[start].append(end)
+            return 1
+        else:
+            return -1    
     def del_edge(self, pair):
+        (one, two) = pair
+        if one in self.nodes and two in self.nodes:
+            self.edges[one].remove(two)
+
+        else:
+            return -1
+
+
+
+
+        #del self.edges[start].append(end)
 
     def children(self):
         return self.edges[node]
@@ -18,11 +35,29 @@ class Graph:
         # We just happen to have an instance variable
         #   the same name as a function 
         return str(self.nodes)
-    def add_node(self):
+    def add_node(self, node):
+        if node in self.nodes:
+            return -1
+        else:
+            self.nodes += [node]
+            self.edges[node] = []
+            return 1
+    def del_node(self, node):
+        if node in self.nodes:
+            self.nodes.remove(node)
+            self.edges.pop(node)
+            for key in self.edges.keys():
+                if node in self.edges[key]:
+                    self.edges[key].remove(node)
+            return 1
+        else:
+            return -1
 
-    def del_node(self):
-
-    def adjMatrix():
+    
+    def adjMatrix(self):
         #prdocues a list of lists that is an N x N matrix
-    def __str__(self):
-        return str(self.edges)
+    
+    
+    #def __str__(self):
+        #return str(self.edges)
+        
